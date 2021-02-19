f52f79fa047748ece0c1072011b507133cb9ceaf
Sam D <samidiaz3@gmail.com>
2021-02-19 23:08:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,11 @@
 class Dog
 
-  def initialize (dog_name)
-    @dog_name = dog_name
+  def name=(dog_name) #setter
+      @dog_name = dog_name
   end
 
+  def name #getter
+    @dog_name
+  end
 
 end
