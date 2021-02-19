0e6676854c0de9a6e31016c505bbb4df6766805a
Sam D <samidiaz3@gmail.com>
2021-02-19 23:12:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  def name=(name) #setter
+      @name = name
+  end
+
+  def name #getter
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+
+end
